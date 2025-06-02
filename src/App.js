e01ecfea07edf78038fa67050cb876651079b798
eunujwal <ujwalc3@gmail.com>
2025-06-01 17:25:43 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// src/App.js
+import React from 'react';
+import StripePricingCalculator from './components/StripePricingCalculator';
+
+function App() {
+  return <StripePricingCalculator />;
+}
+
+export default App;
